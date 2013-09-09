4076fc275c6d945c808c58fbffffd6da92b85b6a
team-sonique <team.sonique.sns@gmail.com>
2013-09-09 15:10:19 +0100

    maven builds extjs to target/superman

--- src/main/java/sonique/bango/store/AgentStore.java
+++ src/main/java/sonique/bango/store/AgentStore.java
@@ -1,11 +1,11 @@
 package sonique.bango.store;
 
-import com.google.common.base.Functions;
 import sonique.bango.domain.Agent;
 import sonique.bango.servlet.GTFOException;
 
 import java.util.Map;
 
+import static com.google.common.base.Functions.forMap;
 import static com.google.common.collect.Maps.newHashMap;
 
 public class AgentStore {
@@ -14,7 +14,7 @@ public class AgentStore {
     private final Map<String, Agent> sessionIdToAgentMap = newHashMap();
 
     public Agent agentFor(String sessionId) {
-        Agent agent = Functions.forMap(sessionIdToAgentMap, null).apply(sessionId);
+        Agent agent = forMap(sessionIdToAgentMap, null).apply(sessionId);
         if (agent == null) {
             throw new GTFOException(403, "No Agent!");
         }
@@ -22,7 +22,12 @@ public class AgentStore {
     }
 
     public void login(String sessionId, String agentCode) {
-        Agent agent = agentCodeToAgentMap.get(agentCode);
+        Agent agent = forMap(agentCodeToAgentMap, null).apply(agentCode);
+
+        if (agent == null) {
+            throw new GTFOException(403, "Unknown Agent!");
+        }
+
         agent.login();
 
         sessionIdToAgentMap.put(sessionId, agent);
