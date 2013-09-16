caad7538cf836e8ef875f9d03dbaffbb7921473f
team-sonique <team.sonique.sns@gmail.com>
2013-09-16 12:59:26 +0100

    reduce duplication in ApiProxies

--- src/main/javascript/app/proxy/ApiOperation.js
+++ src/main/javascript/app/proxy/ApiOperation.js
@@ -3,49 +3,49 @@ Ext.define('Spm.proxy.ApiOperation', {
 
     statics: {
         search: function (options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'read',
                 urlPattern: 'api/search/{0}/{1}',
                 parameterNames: ['searchType', 'searchParameter']
             }, options));
         },
         serviceProblem: function (options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'read',
                 urlPattern: 'api/serviceProblem/{0}',
                 parameterNames: ['serviceProblemId']
             }, options));
         },
         queueServiceProblems: function (options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'read',
                 urlPattern: 'api/queue/{0}/serviceProblems',
                 parameterNames: ['queueId']
             }, options));
         },
         serviceProblemPull: function (options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'update',
                 urlPattern: 'api/serviceProblem/{0}/pull',
                 parameterNames: ['serviceProblemId']
             }, options));
         },
         serviceProblemEventHistory: function(options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'read',
                 urlPattern: 'api/serviceProblem/{0}/eventHistory',
                 parameterNames: ['serviceProblemId']
             }, options));
         },
         eventHistoryAddNote: function(options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'update',
                 urlPattern: 'api/serviceProblem/{0}/eventHistory',
                 parameterNames: ['serviceProblemId']
             }, options));
         },
         agentToggleAvailability: function(options) {
-            return Ext.create('Spm.proxy.ApiOperation', Ext.applyIf({
+            return Ext.create(Spm.proxy.ApiOperation, Ext.applyIf({
                 action: 'update',
                 urlPattern: 'api/agent/toggleAvailability'
             }, options));
