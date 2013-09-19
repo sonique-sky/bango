f1eee1a1947243aab66c69ad53f1348314e5dc3d
Team Sonique <team.sonique.sns@gmail.com>
2013-09-19 10:18:15 +0100

    Adding one level of association binding to AssociationCapableForm.

--- src/main/javascript/app/view/component/AssociationCapableForm.js
+++ src/main/javascript/app/view/component/AssociationCapableForm.js
@@ -3,20 +3,18 @@ Ext.define('Spm.view.component.AssociationCapableForm', {
     alias: 'widget.associationCapableForm',
 
     loadRecord: function (record) {
-
         this.callParent(arguments);
 
-//        this.loadAssociatedData(record);
+        this.loadAssociatedData(record);
     },
 
-    loadAssociatedData: function (record, parentAssociationName) {
+    loadAssociatedData: function (record) {
         var me = this;
 
-        //debugger;
         Ext.Array.forEach(record.associations.items, function (association) {
             var associationGetter = association.getterName;
             var associationRecord = record[associationGetter]();
-            var associationFields = me.query('[name^=' + (parentAssociationName ? parentAssociationName + '.' : '') + association.name + ".]");
+            var associationFields = me.query('[name^=' + association.name + '.]');
 
             Ext.Array.forEach(associationFields, function (field) {
                 var nameTokens = field.name.split('.');
@@ -30,8 +28,6 @@ Ext.define('Spm.view.component.AssociationCapableForm', {
 
                 console.log(field.name+' '+nameToken + ' ' +val);
             });
-
-//            debugger;
         })
     }
 });
