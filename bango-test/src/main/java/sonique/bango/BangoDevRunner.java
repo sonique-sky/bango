ea398d02b5cb409a39dbfd526bf8505550d20c5c
Team Sonique <team.sonique.sns@gmail.com>
2013-09-24 15:56:56 +0100

    Works on Paul's machine...

--- bango-test/src/main/java/sonique/bango/BangoDevRunner.java
+++ bango-test/src/main/java/sonique/bango/BangoDevRunner.java
@@ -3,14 +3,14 @@ package sonique.bango;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-public class BangoRunner {
+public class BangoDevRunner {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
 
         WebAppContext context = new WebAppContext();
         context.setDescriptor("bango-web/src/main/webapp/WEB-INF/web.xml");
-        context.setResourceBase("bango-js/target/superman");
+        context.setResourceBase("bango-js/src/main/javascript");
         context.setContextPath("/superman");
         context.setParentLoaderPriority(true);
 
