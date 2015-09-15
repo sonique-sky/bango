579b3e76ccca5f567cb6a3c72ddf06c0a503c4e8
laksithar <lakshitha.hav@gmail.com>
2015-09-15 22:40:20 +0100

    SON-12 [Sam] enhanced Filter creation and applying of the aggregated filter

--- bango-web/src/main/java/sonique/bango/domain/filter/Filters.java
+++ bango-web/src/main/java/sonique/bango/domain/filter/Filters.java
@@ -6,11 +6,12 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public final class Filters {
-    public static <T> Optional<Predicate<T>> andFilter(List<Filter> filters, Function<Filter, Predicate<T>> generator) {
-        return filters.stream().map(generator::apply).reduce(Predicate::and);
+
+    public static <T> Optional<Predicate<T>> andFilter(List<Filter> filterTerms, Function<Filter, Predicate<T>> generator) {
+        return filterTerms.stream().map(generator::apply).reduce(Predicate::and);
     }
 
-    public static <T> Optional<Predicate<T>> orFilter(List<Filter> filters, Function<Filter, Predicate<T>> generator) {
-        return filters.stream().map(generator::apply).reduce(Predicate::or);
+    public static <T> Optional<Predicate<T>> orFilter(List<Filter> filterTerms, Function<Filter, Predicate<T>> generator) {
+        return filterTerms.stream().map(generator::apply).reduce(Predicate::or);
     }
 }
